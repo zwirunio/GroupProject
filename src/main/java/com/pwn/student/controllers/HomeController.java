f4cfeaf00067cf49b882ec76fdbbd20febd19bde
michal <zwirunio@gmail.com>
2018-04-23 11:08:24 +0200

    HomeController

--- src/main/java/com/pwn/student/controllers/HomeController.java
+++ src/main/java/com/pwn/student/controllers/HomeController.java
@@ -1,4 +1,23 @@
 package com.pwn.student.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/login")
+    public String login() {
+
+        return "loginFormView";
+    }
+
+    @GetMapping("/errorView")
+    public String errorViewHundler() {
+
+        return "errorView";
+    }
+
+
 }
