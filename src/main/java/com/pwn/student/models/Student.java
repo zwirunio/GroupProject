59ab1b3f6d42e030aa7f75a1fc8b41c0ca0f4309
michal <zwirunio@gmail.com>
2018-04-23 12:08:50 +0200

    kolejny Student

--- src/main/java/com/pwn/student/models/Student.java
+++ src/main/java/com/pwn/student/models/Student.java
@@ -39,6 +39,86 @@ public class Student {
         this.rola = "ROLE_USER";
     }
 
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getOcenaProjektu1() {
+        return ocenaProjektu1;
+    }
+
+    public void setOcenaProjektu1(String ocenaProjektu1) {
+        this.ocenaProjektu1 = ocenaProjektu1;
+    }
+
+    public String getOcenaOpisowaProjektu1() {
+        return ocenaOpisowaProjektu1;
+    }
+
+    public void setOcenaOpisowaProjektu1(String ocenaOpisowaProjektu1) {
+        this.ocenaOpisowaProjektu1 = ocenaOpisowaProjektu1;
+    }
+
+    public String getOcenaProjektu2() {
+        return ocenaProjektu2;
+    }
+
+    public void setOcenaProjektu2(String ocenaProjektu2) {
+        this.ocenaProjektu2 = ocenaProjektu2;
+    }
+
+    public String getOcenaOpisowaProjektu2() {
+        return ocenaOpisowaProjektu2;
+    }
+
+    public void setOcenaOpisowaProjektu2(String ocenaOpisowaProjektu2) {
+        this.ocenaOpisowaProjektu2 = ocenaOpisowaProjektu2;
+    }
+
+    public String getOcenaProjektu3() {
+        return ocenaProjektu3;
+    }
+
+    public void setOcenaProjektu3(String ocenaProjektu3) {
+        this.ocenaProjektu3 = ocenaProjektu3;
+    }
+
+    public String getOcenaOpisowaProjektu3() {
+        return ocenaOpisowaProjektu3;
+    }
+
+    public void setOcenaOpisowaProjektu3(String ocenaOpisowaProjektu3) {
+        this.ocenaOpisowaProjektu3 = ocenaOpisowaProjektu3;
+    }
+
+    public String getOcenaProjektu4() {
+        return ocenaProjektu4;
+    }
+
+    public void setOcenaProjektu4(String ocenaProjektu4) {
+        this.ocenaProjektu4 = ocenaProjektu4;
+    }
+
+    public String getOcenaOpisowaProjektu4() {
+        return ocenaOpisowaProjektu4;
+    }
+
+    public void setOcenaOpisowaProjektu4(String ocenaOpisowaProjektu4) {
+        this.ocenaOpisowaProjektu4 = ocenaOpisowaProjektu4;
+    }
+
+    public String getRola() {
+        return rola;
+    }
+
+    public void setRola(String rola) {
+        this.rola = rola;
+    }
+
     public String getName() {
         return name;
     }
@@ -90,12 +170,22 @@ public class Student {
     @Override
     public String toString() {
         return "Student{" +
-                "name='" + name + '\'' +
+                "studentId=" + studentId +
+                ", name='" + name + '\'' +
                 ", lname='" + lname + '\'' +
                 ", email='" + email + '\'' +
                 ", passwd='" + passwd + '\'' +
                 ", gitHub='" + gitHub + '\'' +
                 ", phone='" + phone + '\'' +
+                ", ocenaProjektu1='" + ocenaProjektu1 + '\'' +
+                ", ocenaOpisowaProjektu1='" + ocenaOpisowaProjektu1 + '\'' +
+                ", ocenaProjektu2='" + ocenaProjektu2 + '\'' +
+                ", ocenaOpisowaProjektu2='" + ocenaOpisowaProjektu2 + '\'' +
+                ", ocenaProjektu3='" + ocenaProjektu3 + '\'' +
+                ", ocenaOpisowaProjektu3='" + ocenaOpisowaProjektu3 + '\'' +
+                ", ocenaProjektu4='" + ocenaProjektu4 + '\'' +
+                ", ocenaOpisowaProjektu4='" + ocenaOpisowaProjektu4 + '\'' +
+                ", rola='" + rola + '\'' +
                 '}';
     }
 }
