226c7b1a0a57054f81a2a2e71bd9bd3f8741983f
michal <zwirunio@gmail.com>
2018-04-23 11:02:19 +0200

    Model Student

--- src/main/java/com/pwn/student/models/Student.java
+++ src/main/java/com/pwn/student/models/Student.java
@@ -1,4 +1,93 @@
 package com.pwn.student.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Long   studentId;
+
+    String name;
+    String lname;
+    String email;
+    String passwd;
+    String gitHub;
+    String phone;
+
+    public Student() {
+    }
+
+    public Student(String name, String lname, String email, String passwd, String gitHub, String phone) {
+        this.name = name;
+        this.lname = lname;
+        this.email = email;
+        this.passwd = passwd;
+        this.gitHub = gitHub;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getGitHub() {
+        return gitHub;
+    }
+
+    public void setGitHub(String gitHub) {
+        this.gitHub = gitHub;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", lname='" + lname + '\'' +
+                ", email='" + email + '\'' +
+                ", passwd='" + passwd + '\'' +
+                ", gitHub='" + gitHub + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
